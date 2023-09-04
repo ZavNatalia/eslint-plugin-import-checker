a2252c4d9e320e08b333cddb3a54884a6cabaf5f
NZ <ns@zav.me>
2023-09-04 20:36:00 +0600

    add tests, add option - alias

--- tests/lib/rules/path-checker.js
+++ tests/lib/rules/path-checker.js
@@ -16,16 +16,28 @@ const rule = require("../../../lib/rules/path-checker"),
 // Tests
 //------------------------------------------------------------------------------
 
-const ruleTester = new RuleTester();
+const ruleTester = new RuleTester({
+  parserOptions: {ecmaVersion: 6, sourceType: 'module'}
+});
 ruleTester.run("path-checker", rule, {
   valid: [
-    // give me some code that won't trigger a warning
+    {
+      filename: '/home/natalia/ReactProjects/production-project/src/entities/Article',
+      code: "import { addCommentFormActions, addCommentFormReducer } from '../../model/slices/addCommentFormSlice';",
+      errors: [{ message: "Within a single slice, all paths must be relative" }],
+      options: [
+        {
+          alias: '@',
+        }
+      ]
+    },
   ],
 
   invalid: [
     {
-      code: "asdfg",
-      errors: [{ message: "Fill me in.", type: "Me too" }],
+      filename: '/home/natalia/ReactProjects/production-project/src/entities/Article',
+      code: "import { addCommentFormActions, addCommentFormReducer } from 'entities/Article/model/slices/addCommentFormSlice';",
+      errors: [{ message: "Within a single slice, all paths must be relative" }],
     },
   ],
 });
